72ad169737689181668674e52f50a3ed258ee5f8
Andrew Souza <drewmargielaa@gmail.com>
2020-06-03 20:39:00 -0400

    now writes to .md file

--- index.js
+++ index.js
@@ -1,59 +1,87 @@
-var inquirer = require("inquirer");
-var fs = require("fs");
+// prompt the user
+const inquirer = require('inquirer');
+// make requests
+const api = require('./utils/api.js');
+// build markdown
+const md = require('./utils/generateMarkdown.js');
+// file system
+const fs = require('fs');
 
-inquirer
-  .prompt([
+const questions = [
     {
-      type: "input",
-      message: "What is the Project Title",
-      name: "projecttitle"
+      type: 'input',
+      name: 'username',
+      message: 'What is your Github username?',
     },
     {
-      type: "input",
-      message: "Instructions/ How to get started",
-      name: "instructions"
+      type: 'input',
+      name: 'title',
+      message: 'Project Title?'
     },
     {
-      type: "input",
-      message: "What are the prerequisites",
-      name: "prerequisites"
-    }
+    type: 'input',
+    name: 'version',
+    message: 'Project Version?'
+    },
     {
-      type: "input",
-      message: "How to install",
-      name: "install"
-    }
+      type: 'editor',
+      name: 'description',
+      message: 'Project Description'
+    },
     {
-      type: "input",
-      message: "How to run the tests",
-      name: "tests"
-    }
+      type: 'editor',
+      name: 'installation',
+      message: 'How do you install the project?'
+    },
     {
-      type: "input",
-      message: "Any additional notes",
-      name: "notes"
-    }
-  ])
-  .then(function (response) {
-
-    console.log(response.jobTitle);
-
-
-
-    let readMe_template = `## Read ME ${response.projecttitle},${response.instructions},${response.prerequisites},`;
-    
+      type: 'editor',
+      name: 'usage',
+      message: 'How do you use the project?'
+    },
+    {
+      type: 'input',
+      name: 'license',
+      message: 'Project license?  If applicable '
+    },
+  ];
 
 
-    fs.writeFile("log.txt", readMe_template, function (err) {
+function init() {
+  inquirer
+    .prompt(questions)
+    .then(answers => {
+      api.request(answers.username).then(response => {
+        
+        if(response !== null) {
+          answers.email = response[0];
+          answers.avatar = response[1];
+        }
+        else {
+          console.log('Could not communicate with Github server');
+          return;
+        }
 
-      if (err) {
-        return console.log(err);
-      }
+        
+        const content = md.generateMarkdown(answers);
 
-      console.log("Success!");
+        // write the data to a file
+        writeToFile('README.md', content);
+      });
 
     });
 
 
-    
+}
+
+function writeToFile(fileName, data) {
+
+  fs.writeFile(fileName, data, function(error) {
+    if(error) console.log(error);
+    else console.log('Success!');
   });
+
+
+}
+
+// run program
+init();
